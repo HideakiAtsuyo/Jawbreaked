6d88bf2075991a550d57a8e130b7f46068306898
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-10-05 21:11:28 +0200

    Add example
    - input.py = Script To Protect
    - output.py = Script Protected
    - Deobfuscated.py = Deobfuscated output.py

---
+++ Example/Deobfuscated.py
@@ -0,0 +1,10 @@
+# Oofed with https://github.com/HideakiAtsuyo/Jawbreaked
+def printThis(arg: str):
+    print(arg)
+
+def Init():
+    printThis("Hello World !");
+
+
+if __name__ == '__main__':
+    Init()
---
+++ Example/input.py
@@ -0,0 +1,9 @@
+def printThis(arg: str):
+    print(arg)
+
+def Init():
+    printThis("Hello World !");
+
+
+if __name__ == '__main__':
+    Init()
